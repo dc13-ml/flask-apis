3d6fd5f11f5f7d6ddcf78d925948fe85b494738e
dennis cheng <c_n_cheng@yahoo.com>
2018-09-23 18:36:18 -0500

    Fix bug to run on Heroku

--- app.py
+++ app.py
@@ -13,11 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "dkeitheidkd;"
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    # based on the 'SQLALCHEMY_DATABASE_URI' config parameter
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Item, '/item/<string:name>')
